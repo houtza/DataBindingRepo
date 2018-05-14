f34e61c982a22ebb3924ca71ef8bc2e64caccef6
intern2017 <ahoutz@vermeer.com>
2018-05-13 19:34:04 -0500

    Test

---
+++ DataBinding/DataBinding/DataBinding/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace DataBinding.Models
+{
+    class Customer : INotifyPropertyChanged
+    {
+        public Customer(String customerName)
+        {
+            Name = customerName;
+        }
+
+        private String _Name;
+
+        public String Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                _Name = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(String propteryName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler != Null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
